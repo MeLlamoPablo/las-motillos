7c52acd3612737f2d2370a4de9538d341c79152a
Pablo Rodríguez Caballero <pablo@rodriguezcaballero.me>
2023-01-28 12:18:21 +0100

    Add automated script to cache regions

---
+++ acciona-client/scripts/cacheRegions.ts
@@ -0,0 +1,23 @@
+import { writeFile } from "fs/promises";
+import { join } from "path";
+import { getPublicAccionaClient } from "../src";
+
+async function main() {
+  const acciona = await getPublicAccionaClient();
+  const regions = await acciona.getRegions();
+
+  await writeFile(
+    join(__dirname, "../assets/regions.json"),
+    JSON.stringify(regions),
+    "utf-8"
+  );
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
